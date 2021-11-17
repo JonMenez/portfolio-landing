64b65f3082ed8c009d733230ea9d2788fc282f13
Joan Gimenez <joanf.gimenez@gmail.com>
2021-11-17 16:54:27 -0300

    [ADD] Profile component

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,11 +1,23 @@
 import React from 'react'
 import Header from './Header'
+import Profile from '../components/Profile';
 import '../styles/app.scss'
+import { Fragment } from 'react';
+import Hexagon from '../components/Hexagon';
+
 
 
 const App = () => {
     return (
-           <Header/>    
+        <Fragment>    
+        
+
+           <Header />  
+           <Profile />
+           <Hexagon/>
+      
+
+        </Fragment>
     )
 }
 
