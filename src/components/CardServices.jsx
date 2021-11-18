45871c95ab34dfb83fe8ae1b50e7fbf745541f1e
Joan Gimenez <joanf.gimenez@gmail.com>
2021-11-18 08:09:18 -0300

    [ADD] Services container & CardServiceComponent

---
+++ src/components/CardServices.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CardServices = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default CardServices
---
+++ src/containers/Services.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import CardServices from '../components/CardServices'
+
+const Services = () => {
+    return (
+        <section>
+            <CardServices/>
+        </section>
+    )
+}
+
+export default Services
