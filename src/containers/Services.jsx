9223fd701b537cf61589075674b4652ac59e4b7d
Joan Gimenez <joanf.gimenez@gmail.com>
2021-11-22 20:38:17 -0300

    [ADD] CardServices component

--- src/containers/Services.jsx
+++ src/containers/Services.jsx
@@ -1,11 +1,15 @@
 import React        from 'react'
 import CardServices from '@components/CardServices'
+import {datas}  from "@components/ServicesList.js";
+import '@styles/services.scss';
 
 const Services = () => {
     return (
         <section className="services">
-            <h2>How can i help you?</h2>
-            <CardServices/>
+            <div className="services__card">
+              <h2 className="services__main">How can i help you?</h2>
+              { datas.map(data => <CardServices data={data} key={data.id} />) }
+            </div>
         </section>
     )
 }
