7748a62dde8c531e157393cec8433d41c45d9faf
Joan Gimenez <joanf.gimenez@gmail.com>
2021-11-22 20:40:09 -0300

    [ADD] list of services object

---
+++ src/components/ServicesList.js
@@ -0,0 +1,37 @@
+import FrontIcon from "@icons/code-block.svg"
+
+export  const datas = [
+    {
+        id: 1,
+        title: "Front end",
+        info: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque mollis ante vel odio maximus, facilisis egestas mi blandit. ",
+        image: FrontIcon
+
+    },
+    {
+        id: 2,
+        title: "Front end",
+        info: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque mollis ante vel odio maximus, facilisis egestas mi blandit. ",
+        image: FrontIcon
+
+    },
+    {
+        id: 3,
+        title: "Front end",
+        info: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque mollis ante vel odio maximus, facilisis egestas mi blandit. ",
+        image: FrontIcon
+
+    },
+    {
+        id: 4,
+        title: "Front end",
+        info: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque mollis ante vel odio maximus, facilisis egestas mi blandit. ",
+        image: FrontIcon
+
+    },
+    
+]
+
+
+
+
