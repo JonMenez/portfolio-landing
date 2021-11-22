133309f62135d56d16cc8736a78cf3b492a9d2e8
Joan Gimenez <joanf.gimenez@gmail.com>
2021-11-22 20:41:46 -0300

    [ADD] Services component

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -2,6 +2,7 @@ import React        from 'react'
 import { Fragment } from 'react';
 import Header       from './Header'
 import Profile      from '@components/Profile';
+import Services     from './Services';
 import '@styles/app.scss'
 
 
@@ -12,7 +13,7 @@ const App = () => {
         
            <Header />  
            <Profile />
-      
+           <Services/>
         </Fragment>
     )
 }
