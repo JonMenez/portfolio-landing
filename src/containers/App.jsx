7654f1b8145a3634a762ec6eb319b33829398b05
Joan Gimenez <joanf.gimenez@gmail.com>
2021-12-07 15:18:35 -0300

    [UPDATE] Feedback's route

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -5,8 +5,8 @@ import Profile      from '@containers/Profile';
 import Services     from '@containers/Services';
 import Welcome      from '@containers/Welcome';
 import Portfolio    from '@containers/Portfolio';
+import Feedbacks    from '@containers/Feedbacks';
 import '@styles/app.scss';
-import Feedbacks from './Feedbacks';
 
 
 
