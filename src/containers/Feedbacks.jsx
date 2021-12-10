d3d53a483e3e4c0ef4accf8d1949bfa6d9dc1f8e
Joan Gimenez <joanf.gimenez@gmail.com>
2021-12-10 10:39:32 -0300

    [ADD] Swiper Component & config

--- src/containers/Feedbacks.jsx
+++ src/containers/Feedbacks.jsx
@@ -1,14 +1,36 @@
-import * as React from 'react'
-import CardFeed from '@components/CardFeed'
-import '@styles/feedbacks.scss'
+import * as React from 'react';
+import CardFeed from '@components/CardFeed';
+import wave from '@images/wave.svg';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Pagination, Navigation} from 'swiper';
+import 'swiper/scss';
+import 'swiper/scss/pagination';
+import 'swiper/scss/navigation';
+import '@styles/feedbacks.scss';
 
 const Feedbacks = () => {
+
     return (
         <section className="feedbacks">
             <h4 className="feedbacks__title feedbacks__title--main">We really care about what you have to say!</h4>
-            <CardFeed />
+            <Swiper
+            modules={[ Pagination, Navigation]}
+            spaceBetween={50}
+            slidesPerView={1}
+            tag="div"
+            navigation
+            pagination={{ clickable: true }} 
+            className="feedbacks__swiper"
+            id="feedbacks__swiper"
+              >
+      <SwiperSlide><CardFeed /></SwiperSlide>
+      <SwiperSlide><CardFeed /></SwiperSlide>
+      <SwiperSlide><CardFeed /></SwiperSlide>
+      <SwiperSlide><CardFeed /></SwiperSlide>
+    </Swiper>
             <h4 className="feedbacks__title">every opinion we receive help us a lot</h4>
-        </section>
+            <img className="feedbacks__wave" src={ wave } alt="wave shape" />
+            </section>
     )
 }
 
