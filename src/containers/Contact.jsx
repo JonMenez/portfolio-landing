7ee8ee877c75240a1ec7794523c3d46fb51d960d
Joan Gimenez <joanf.gimenez@gmail.com>
2021-12-10 11:30:21 -0300

    [ADD] Contact container

---
+++ src/containers/Contact.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Contact = () => {
+    return (
+        <section>
+            <h6>Now is the time to meet us and make all your work have your brand</h6>
+            <p>I just need a few details, it will take <span>less than a minute</span></p>
+        </section>
+    )
+}
+
+export default Contact
