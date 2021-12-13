0dac8e148a516d7919efeb3697dab71b4f9bfbfa
Joan Gimenez <joanf.gimenez@gmail.com>
2021-12-13 16:32:25 -0300

    [UPDATE] wave SVG moved to here

--- src/containers/Contact.jsx
+++ src/containers/Contact.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
+import React from 'react';
+import wave from '@images/wave.svg';
+import '@styles/contact.scss';
 
 const Contact = () => {
     return (
-        <section>
-            <h6>Now is the time to meet us and make all your work have your brand</h6>
-            <p>I just need a few details, it will take <span>less than a minute</span></p>
+        <section className="contact">
+            <img src={ wave } alt="wave shape" />
+            <h6 className="contact__title">Now is time to meet us and make all your work have your brand</h6>
+            <p className="contact__text">I just need a few details, it will take <strong>less than a minute</strong></p>
         </section>
     )
 }
